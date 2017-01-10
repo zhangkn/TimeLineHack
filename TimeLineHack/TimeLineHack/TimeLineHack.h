dff726278a1267b7cc2b0f77f38b761eafd932e2
MiracleF <13660180195@163.com>
2017-01-10 23:39:01 +0800

    第一次

---
+++ TimeLineHack/TimeLineHack/TimeLineHack.h
@@ -0,0 +1,13 @@
+//
+//  TimeLineHack.h
+//  TimeLineHack
+//
+//  Created by Jay on 2017/1/10.
+//  Copyright (c) 2017年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TimeLineHack : NSObject
+
+@end
---
+++ TimeLineHack/TimeLineHack/TimeLineHack.m
@@ -0,0 +1,22 @@
+//
+//  TimeLineHack.m
+//  TimeLineHack
+//
+//  Created by Jay on 2017/1/10.
+//  Copyright (c) 2017年 __MyCompanyName__. All rights reserved.
+//
+
+#import "TimeLineHack.h"
+
+@implementation TimeLineHack
+
+-(id)init
+{
+	if ((self = [super init]))
+	{
+	}
+    
+	return self;
+}
+
+@end
